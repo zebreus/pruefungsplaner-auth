8581f7eca98246dc4cddb7593e971e8883005da0
Lennart Eichhorn <lennart@madmanfred.com>
2019-11-13 13:19:00 +0100

    Added first thrift service definition.

--- res/SecurityProvider.thrift
+++ res/SecurityProvider.thrift
@@ -1,1 +1,22 @@
 
+#!/usr/local/bin/thrift --gen cpp
+
+namespace cpp SecurityProviderThrift
+
+exception InvalidClaim {
+1: string message,
+}
+
+exception UnauthorizedClaim {
+1: string message,
+}
+
+exception InvalidAuthorization {
+1: string message,
+}
+
+service CertificateGenerator {
+  void authorize(1:string username, 2:string password),
+  void requestClaim(1:string claim),
+  string getToken()
+}
